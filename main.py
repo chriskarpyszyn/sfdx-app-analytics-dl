9611262c678fe4850aee3eae3c081293d396c908
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-02-28 11:08:09 -0500

    feat: stub salesforce authentication

---
+++ main.py
@@ -0,0 +1,14 @@
+from simple_salesforce import Salesforce
+
+
+def main():
+    # login to salesforce
+    sf_instance = Salesforce(username='', password='', security_token='', domain='')
+
+    # get a list of org ids
+
+    # create app analytics query request
+
+    # wait, get record w/ aws link
+
+    # save csv from aws
