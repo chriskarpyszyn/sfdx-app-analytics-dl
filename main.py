43fc18b7541149e3b099f9ecb0171f67e7f54d21
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-03-01 10:26:54 -0500

    feat: get a comma delimited string of org ids for my license

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from simple_salesforce import Salesforce
 from dotenv import load_dotenv
 from pathlib import Path
 
+
 # todo move to config file
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
@@ -12,13 +13,39 @@ sfdc_token = os.getenv('SFDC_TOKEN')
 
 
 def main():
+    print('hi mom')
     # login to salesforce
     sf_instance = Salesforce(username=sfdc_username, password=sfdc_password, security_token=sfdc_token)
 
-    # get a list of org ids
+    org_id_string = get_org_ids_string(sf_instance)
+    print(org_id_string)
 
     # create app analytics query request
 
     # wait, get record w/ aws link
 
     # save csv from aws
+
+
+def get_org_ids_string(sf_instance):
+    """
+    query salesforce to get a comma delimited string of org ids
+    :param sf_instance: the sf instance to query from
+    :return: comma delimited string of org ids
+    """
+    # this where condition is specific to my use case.
+    # todo extract the hardcoded variable
+    where_condition = "WHERE sfLma__Package_Version__r.Name LIKE 'v%1.%'"
+    data = sf_instance.query("SELECT sfLma__Subscriber_Org_ID__c FROM sfLma__License__c " + where_condition)
+
+    org_ids = set()
+    for k, v in data.items():
+        if k == "records":
+            for item in v:
+                org_ids.add(item.get('sfLma__Subscriber_Org_ID__c'))
+
+    org_ids_string = ','.join(str(s) for s in org_ids)
+    return org_ids_string
+
+
+main()
