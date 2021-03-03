f251c1439dc99b37325b35fa662ff333cba17846
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-03-03 09:55:54 -0500

    refactor: create method for getting the csv url
    breaking change, organization id string is too large

--- main.py
+++ main.py
@@ -17,22 +17,35 @@ def main():
     # login to salesforce
     sf_instance = Salesforce(username=sfdc_username, password=sfdc_password, security_token=sfdc_token, version=50.0)
 
+    # get a comma delimited list of organization ids
     org_id_string = get_org_ids_string(sf_instance)
+
+    # create the app analytics record and get the id
     id_of_analytics_record = create_app_analytic_record(sf_instance, sfdc_package_ids, org_id_string)
 
     # wait, get record w/ aws link
-    # todo refactor into method
-    app_data = sf_instance.AppAnalyticsQueryRequest.get(id_of_analytics_record)
-    request_state = app_data.get('RequestState')
+    csv_url = get_csv_url(sf_instance, id_of_analytics_record)
+    print(csv_url)
+
+    # save csv from aws
+
+
+def get_csv_url(sf_instance, app_analytics_id):
+    """
+    Given an App Analytics record Id, poll Salesforce until the CSV is ready to be retrieved
+    :param sf_instance:
+    :param app_analytics_id:
+    :return:
+    """
+    aaqr_record = sf_instance.AppAnalyticsQueryRequest.get(app_analytics_id)
+    request_state = aaqr_record.get('RequestState')
     while request_state == 'New' or request_state == 'Pending':
         print(request_state)
         print('waiting')
         time.sleep(5)
 
-    csv_url = app_data.get('DownloadUrl')
-    print(csv_url)
-
-    # save csv from aws
+    csv_url = aaqr_record.get('DownloadUrl')
+    return csv_url
 
 
 def create_app_analytic_record(sf_instance, packageIds, organizationIds):
@@ -44,6 +57,7 @@ def create_app_analytic_record(sf_instance, packageIds, organizationIds):
     :return: the id of the successfully created record
     """
 
+    # todo set date string to yesterday
     app_analytics_response = sf_instance.AppAnalyticsQueryRequest.create({
         'DataType': 'PackageUsageLog',
         'StartTime': '2021-03-01T00:00:00',
@@ -64,7 +78,7 @@ def get_org_ids_string(sf_instance):
     """
     # this where condition is specific to my use case.
     # todo extract the hardcoded variable
-    where_condition = "WHERE sfLma__Package_Version__r.Name LIKE 'v%1.%'"
+    where_condition = "WHERE sfLma__Package_Version__r.Name LIKE 'v%1.%'" + " AND sfLma__Is_Sandbox__c = FALSE"
     data = sf_instance.query("SELECT sfLma__Subscriber_Org_ID__c FROM sfLma__License__c " + where_condition)
 
     org_ids = set()
