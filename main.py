e8a49b60fdddaa1941c2593b472caab212584640
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2021-03-03 11:45:19 -0500

    fix: split request into chunks of organization ids
    field length is max 255, so approx 15 org ids per batch

--- main.py
+++ main.py
@@ -18,10 +18,24 @@ def main():
     sf_instance = Salesforce(username=sfdc_username, password=sfdc_password, security_token=sfdc_token, version=50.0)
 
     # get a comma delimited list of organization ids
-    org_id_string = get_org_ids_string(sf_instance)
+    all_org_ids_string = get_org_ids_string(sf_instance)
+
+    # string into list
+    list_of_org_ids = all_org_ids_string.split(",")
+
+    # create a list of lists with 15 ids.
+    list_of_org_id_strings = []
+    index_of_list = -1
+    for i in range(0, len(list_of_org_ids)):
+        if i % 15 == 0:
+            index_of_list = index_of_list + 1
+            list_of_org_id_strings.append([])
+        # list_of_org_id_strings[index_of_list].append(list_of_org_ids[i])
+        list_of_org_id_strings[index_of_list] += [list_of_org_ids[i]]
+    print(list_of_org_id_strings)
 
     # create the app analytics record and get the id
-    id_of_analytics_record = create_app_analytic_record(sf_instance, sfdc_package_ids, org_id_string)
+    id_of_analytics_record = create_app_analytic_record(sf_instance, sfdc_package_ids, list_of_org_id_strings[0])
 
     # wait, get record w/ aws link
     csv_url = get_csv_url(sf_instance, id_of_analytics_record)
@@ -48,22 +62,24 @@ def get_csv_url(sf_instance, app_analytics_id):
     return csv_url
 
 
-def create_app_analytic_record(sf_instance, packageIds, organizationIds):
+def create_app_analytic_record(sf_instance, package_ids, organization_ids):
     """
     Create a Package Usage Log App Analytics Query Request for the previous day
     :param sf_instance
-    :param packageIds:
-    :param organizationIds:
+    :param package_ids:
+    :param organization_ids:
     :return: the id of the successfully created record
     """
+
+    comma_delimited_string = ','.join(str(s) for s in organization_ids)
 
     # todo set date string to yesterday
     app_analytics_response = sf_instance.AppAnalyticsQueryRequest.create({
         'DataType': 'PackageUsageLog',
         'StartTime': '2021-03-01T00:00:00',
         'EndTime': '2021-03-01T23:59:59',
-        'PackageIds': packageIds,
-        'OrganizationIds': organizationIds
+        'PackageIds': package_ids,
+        'OrganizationIds': comma_delimited_string
     })
 
     # todo check if successful
@@ -78,7 +94,7 @@ def get_org_ids_string(sf_instance):
     """
     # this where condition is specific to my use case.
     # todo extract the hardcoded variable
-    where_condition = "WHERE sfLma__Package_Version__r.Name LIKE 'v%1.%'" + " AND sfLma__Is_Sandbox__c = FALSE"
+    where_condition = "WHERE sfLma__Package_Version__r.Name LIKE 'v%1.%'"
     data = sf_instance.query("SELECT sfLma__Subscriber_Org_ID__c FROM sfLma__License__c " + where_condition)
 
     org_ids = set()
